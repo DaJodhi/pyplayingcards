16c15922300669d9cbf7818c410e196a2a4bd6b1
Jodhviir Sekhon <49446509+DaJodhi@users.noreply.github.com>
2020-05-13 09:01:09 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pyplayingcards",
-    version="1.1.4",
+    version="1.2.4",
     description="Create and manipulate playing cards in Python!",
     long_description=README,
     long_description_content_type="text/markdown",
